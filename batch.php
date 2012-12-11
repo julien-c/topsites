59111899964663cd24b11613f7b2b318b35b60cb
Julien Chaumond <chaumond@gmail.com>
2012-12-11 19:53:45 +0100

    Batch script : Top 1-30.000 FR

---
+++ batch.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'topsites.class.php';
+
+$accessKeyId = $argv[1];
+$secretAccessKey = $argv[2];
+
+for ($i = 0; $i < 300; $i++) {
+	$start = $i * 100 + 1;
+	$topSites = new TopSites($accessKeyId, $secretAccessKey, 'FR', $start);
+	$topSites->getTopSites();
+}
